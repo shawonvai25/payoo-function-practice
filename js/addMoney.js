9c548e38c365f26356aa2adfe38c933012127088
shawonvai25 <abdullahalshawon25@gmail.com>
2024-11-05 23:23:28 +0600

    added add money form

---
+++ js/addMoney.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-add-money')
+      .addEventListener('click',function(event){
+         event.preventDefault();
+
+        const addMoney = getInputFieldValueById('input-add-money');
+        const pinNumber = getInputFieldValueById('add-money-pin');
+
+        console.log('Money added amount:',addMoney)
+        console.log('The pin number is:',pinNumber)
+        
+    // wrong way to verify. Please don't use in your serious project
+       if(pinNumber === 1234){
+        const balance = textFieldValueById('account-balance');
+        const newBalance = balance + addMoney ;
+        console.log(newBalance);
+
+        document.getElementById('account-balance').innerText = newBalance ;
+       }
+
+
+
+
+})
---
+++ js/utilities.js
@@ -0,0 +1,13 @@
+function getInputFieldValueById(id){
+
+    const inputValue = document.getElementById(id).value ;
+    const inputNumber = parseFloat(inputValue);
+     return inputNumber;
+}
+
+
+function textFieldValueById(id){
+    const textValue = document.getElementById(id).innerText ;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+}
